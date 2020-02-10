2623bc391ffd8bb7469f3a0cd0f2f3586a885bed
MikkoNirkko <abekebab@gmail.com>
2020-02-10 22:55:15 +0200

    initial commit

---
+++ functions.js
@@ -0,0 +1,41 @@
+function getBikes(bike_id, titleTarget, statusTarget, colorTarget){
+      fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: '{bikeRentalStation(id:"'+bike_id+'") { name bikesAvailable spacesAvailable}}'}),
+      })
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(myJson) {
+          var stationName = myJson.data.bikeRentalStation.name;
+          /*var bikesAvailable = myJson.data.bikeRentalStation.bikesAvailable;
+          var spacesAvailable = myJson.data.bikeRentalStation.spacesAvailable;
+          var totalAvailable = bikesAvailable+spacesAvailable;*/
+          var bikesAvailable = 3;
+          var totalAvailable = 10;
+          var color = "";
+          if(bikesAvailable<=Math.round(totalAvailable/3) && bikesAvailable > 0){
+            color = "niceText orange"
+          } else if(bikesAvailable>=Math.round(totalAvailable/3)){
+              color = "niceText green"
+          } else {
+              color="niceText red"
+          }
+          document.getElementById(colorTarget).className = color;
+          document.getElementById(titleTarget).innerHTML=stationName;
+          document.getElementById(statusTarget).innerHTML=bikesAvailable+" / "+totalAvailable;
+      });
+  }
+  
+  function initializeData(ms=15000){
+      updateData()
+      setInterval(function(){
+        updateData()
+    }, ms);
+  }
+
+  function updateData(){
+        getBikes("256", "stationTitle1", "stationStatus1", "bikeblock1");
+        getBikes("259", "stationTitle2", "stationStatus2", "bikeblock2");
+  }
