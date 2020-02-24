e1098fb2b154129ccf93ec9eb839809a3121e14c
MikkoNirkko <abekebab@gmail.com>
2020-02-24 19:42:49 +0200

    new sw path

--- js/functions.js
+++ js/functions.js
@@ -79,7 +79,7 @@ function initializeData(ms = 15000) {
     });
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
-            navigator.serviceWorker.register('sw.js').then(function(registration) {
+            navigator.serviceWorker.register('js/sw.js').then(function(registration) {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             }, function(err) {
